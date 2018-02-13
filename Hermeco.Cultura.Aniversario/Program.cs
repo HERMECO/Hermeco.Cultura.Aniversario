2004a51053f85a47d4c3d9a70d347310bcbe9497
jacksson.mosquera@offcorss.com <1@offcorss.com>
2018-02-13 14:38:10 -0500

    Add project files.

---
+++ Hermeco.Cultura.Aniversario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hermeco.Cultura.Aniversario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
