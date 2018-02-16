c53f4c8316d4c4a773d2237bc031b11855e05b84
jacksson.mosquera@offcorss.com <1@offcorss.com>
2018-02-16 14:59:07 -0500

    Primera carga

--- Hermeco.Cultura.Aniversario/Program.cs
+++ Hermeco.Cultura.Aniversario/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace Hermeco.Cultura.Aniversario
 {
     class Program
     {
+        public static IConfiguration Configuration { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            wsEmpleados.EmpleadosSAPServiceClient client = new wsEmpleados.EmpleadosSAPServiceClient();
+            Task<wsEmpleados.GetEmployeeIntranetResponse> response = client.GetAnniversariesAsync();
+
+            string fromEmail = Configuration["FromEmail"].ToString();
+            string smtpServer = Configuration["SMTPServer"].ToString();
+            string subject = string.Empty;
+            
+
+            foreach (wsEmpleados.EmployeeIntranet empleado in response.Result.EmployeeIntranet)
+            {
+                try
+                {
+                    subject = string.Format("¡{0}, Feliz Aniversario!", empleado.FirstName.Trim().Split(' ')[0]);
+                    if (!string.IsNullOrEmpty(empleado.EMail))
+                    {                        
+                        Utility.sendEmail(smtpServer, subject, "Email\\Aniversario.html", "", fromEmail, empleado.EMail, "", true, "", null);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            
         }
     }
 }
---
+++ Hermeco.Cultura.Aniversario/Utility.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mail;
+
+namespace Hermeco.Cultura.Aniversario
+{
+    internal class Utility
+    {
+        internal static void sendEmail(string smtpServer, string subject, string htmlPath, string body, string from, string to, string pathAttachment, bool isBodyHtml, string CC, Dictionary<string, string> parameters = null)
+        {
+
+            MailMessage mail = new MailMessage(from, to) { From = new MailAddress(from, "Yo Soy Cultura OFFCORSS"), IsBodyHtml = isBodyHtml, Subject = subject };
+
+            SmtpClient client = new SmtpClient() { Port = 25, DeliveryMethod = SmtpDeliveryMethod.Network, UseDefaultCredentials = false, Host = smtpServer };
+
+            if (isBodyHtml)
+            {
+                mail.IsBodyHtml = true;
+
+                using (StreamReader reader = File.OpenText(htmlPath))
+                {
+                    var inlineLogo = new LinkedResource(AppDomain.CurrentDomain.BaseDirectory + "Email//img//tarjeta-Aniversario-.jpg");
+                    inlineLogo.ContentId = Guid.NewGuid().ToString();
+                    string body2 = string.Format(@reader.ReadToEnd(), inlineLogo.ContentId);
+                    var view = AlternateView.CreateAlternateViewFromString(body2, null, "text/html");
+                    view.LinkedResources.Add(inlineLogo);
+                    mail.AlternateViews.Add(view);
+                }
+            }
+            else
+            {
+                mail.Body = body;
+            }
+
+            if (!string.IsNullOrEmpty(pathAttachment))
+            {
+                mail.Attachments.Add(new Attachment(pathAttachment));
+            }
+
+            if (!string.IsNullOrEmpty(CC))
+            {
+                var MailCC = new MailAddress(CC);
+                mail.CC.Add(MailCC);
+            }
+
+            client.Send(mail);
+        }
+    }
+}
